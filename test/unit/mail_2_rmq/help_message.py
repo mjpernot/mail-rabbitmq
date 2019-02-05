f56774016330ae0e9e3fbc121a1fa46160ae9d83
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-02-05 18:07:51 +0000

    Added test/unit/mail_2_rmq/help_message.py

---
+++ test/unit/mail_2_rmq/help_message.py
@@ -0,0 +1,85 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  help_message.py
+
+    Description:  Unit testing of help_message in mail_2_rmq.py.
+
+    Usage:
+        test/unit/mail_2_rmq/help_message.py
+
+    Arguments:
+        None
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+import mock
+
+# Local
+sys.path.append(os.getcwd())
+import mail_2_rmq
+import lib.gen_libs as gen_libs
+import version
+
+# Version
+__version__ = version.__version__
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Super-Class:  unittest.TestCase
+
+    Sub-Classes:  None
+
+    Methods:
+        setUp -> Unit testing initilization.
+        test_help_message -> Test help message.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+            None
+
+        """
+
+        pass
+
+    def test_help_message(self):
+
+        """Function:  test_help_message
+
+        Description:  Test help message.
+
+        Arguments:
+            None
+
+        """
+
+        with gen_libs.no_std_out():
+            self.assertFalse(mail_2_rmq.help_message())
+
+
+if __name__ == "__main__":
+    unittest.main()
