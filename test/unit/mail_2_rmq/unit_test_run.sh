7271ae63a2b2157e5ee396aa86209fb67a836c76
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-02-05 11:21:26 +0000

    Updated test/unit/mail_2_rmq/unit_test_run.sh

--- test/unit/mail_2_rmq/unit_test_run.sh
+++ test/unit/mail_2_rmq/unit_test_run.sh
@@ -4,10 +4,11 @@
 # Will need to run this from the base directory where the class file 
 #   is located at.
 
-cat test/unit/mail_2_rmq/test_mail.txt | test/unit/mail_2_rmq/test_parse_email.py
-test/unit/mail_2_rmq/test_load_cfg.py
-test/unit/mail_2_rmq/test_archive_email.py
-cat test/unit/mail_2_rmq/test_mail.txt | test/unit/mail_2_rmq/test_connect_process.py
-cat test/unit/mail_2_rmq/test_mail.txt | test/unit/mail_2_rmq/test_process_message.py -G
-cat test/unit/mail_2_rmq/test_mail2.txt | test/unit/mail_2_rmq/test_process_message.py -B
-test/unit/mail_2_rmq/mail_2_rmq_cleanup.py
+test/unit/mail_2_rmq/load_cfg.py
+
+#cat test/unit/mail_2_rmq/test_mail.txt | test/unit/mail_2_rmq/test_parse_email.py
+#test/unit/mail_2_rmq/test_archive_email.py
+#cat test/unit/mail_2_rmq/test_mail.txt | test/unit/mail_2_rmq/test_connect_process.py
+#cat test/unit/mail_2_rmq/test_mail.txt | test/unit/mail_2_rmq/test_process_message.py -G
+#cat test/unit/mail_2_rmq/test_mail2.txt | test/unit/mail_2_rmq/test_process_message.py -B
+##test/unit/mail_2_rmq/mail_2_rmq_cleanup.py
