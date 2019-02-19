afabd7a54f8a278d2343c919b02f28b0cf2b2e12
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-02-19 12:18:07 +0000

    Updated test/unit/mail_2_rmq/get_text.py

--- test/unit/mail_2_rmq/get_text.py
+++ test/unit/mail_2_rmq/get_text.py
@@ -48,6 +48,13 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Unit testing initilization.
+        test_empty_msg -> Test empty message is returned.
+        test_single_part_msg -> Test single part message is returned.
+        test_two_part_msg -> Test two part message is returned.
+        test_multi_part_msg -> Test multi-part message is returned.
+        test_empty_payload_msg -> Test when payload returns empty.
+        test_multipart_empty_payload_msg -> Test multipart & empty payload msg.
+        test_multiple_part_msg -> Test returning multiple part messages.
 
     """
 
@@ -74,6 +81,7 @@ class UnitTest(unittest.TestCase):
 
             Methods:
                 __init__ -> Initialize configuration environment.
+                walk -> Stub holder for walk method.
 
             """
 
@@ -103,7 +111,184 @@ class UnitTest(unittest.TestCase):
 
                 return self.walk_list
 
+        class PartMsgTest(object):
+
+            """Class:  PartMsgTest
+
+            Description:  Is a representation of a part of an email message
+                instance.
+
+            Super-Class:  object
+
+            Sub-Classes:  None
+
+            Methods:
+                __init__ -> Initialize configuration environment.
+                get_content_maintype -> Stub holder for get_content_maintype.
+                get_payload -> Stub holder for get_payload method.
+                
+
+            """
+
+            def __init__(self):
+
+                """Method:  __init__
+
+                Description:  Initialization instance of the RQTest class.
+
+                Arguments:
+                        None
+
+                """
+
+                self.type = ""
+                self.payload = ""
+
+            def get_content_maintype(self):
+
+                """Method:  get_content_maintype
+
+                Description:  Stub holder for get_content_maintype method.
+
+                Arguments:
+                        None
+
+                """
+
+                return self.type
+
+            def get_payload(self, decode):
+
+                """Method:  get_payload
+
+                Description:  Stub holder for get_payload method.
+
+                Arguments:
+                        decode = Place holder for decode variable.
+
+                """
+
+                return self.payload
+
         self.MSG = MsgTest()
+        self.PART_MSG = PartMsgTest()
+        self.PART_MSG2 = PartMsgTest()
+        self.PART_MSG3 = PartMsgTest()
+        self.PART_MSG4 = PartMsgTest()
+
+    def test_multiple_part_msg(self):
+
+        """Function:  test_multiple_part_msg
+
+        Description:  Test returning multiple part messages.
+
+        Arguments:
+            None
+
+        """
+
+        self.MSG.walk_list = [self.PART_MSG, self.PART_MSG2, self.PART_MSG3,
+                              self.PART_MSG4]
+        self.PART_MSG.type = "multipart"
+        self.PART_MSG2.type = "single"
+        self.PART_MSG2.payload = "Email"
+        self.PART_MSG3.type = "single"
+        self.PART_MSG3.payload = ""
+        self.PART_MSG4.type = "single"
+        self.PART_MSG4.payload = " Message"
+
+        self.assertEqual(mail_2_rmq.get_text(self.MSG), "Email Message")
+
+    def test_multipart_empty_payload_msg(self):
+
+        """Function:  test_multipart_empty_payload_msg
+
+        Description:  Test multipart and empty payload msg.
+
+        Arguments:
+            None
+
+        """
+
+        self.MSG.walk_list = [self.PART_MSG, self.PART_MSG2, self.PART_MSG3]
+        self.PART_MSG.type = "multipart"
+        self.PART_MSG2.type = "single"
+        self.PART_MSG2.payload = ""
+        self.PART_MSG3.type = "single"
+        self.PART_MSG3.payload = "Email"
+
+        self.assertEqual(mail_2_rmq.get_text(self.MSG), "Email")
+
+    def test_empty_payload_msg(self):
+
+        """Function:  test_empty_payload_msg
+
+        Description:  Test when payload returns empty.
+
+        Arguments:
+            None
+
+        """
+
+        self.MSG.walk_list = [self.PART_MSG, self.PART_MSG2]
+        self.PART_MSG.type = "single"
+        self.PART_MSG.payload = ""
+        self.PART_MSG2.type = "single"
+        self.PART_MSG2.payload = "Email"
+
+        self.assertEqual(mail_2_rmq.get_text(self.MSG), "Email")
+
+    def test_multi_part_msg(self):
+
+        """Function:  test_multi_part_msg
+
+        Description:  Test multi-part message is returned.
+
+        Arguments:
+            None
+
+        """
+
+        self.MSG.walk_list = [self.PART_MSG, self.PART_MSG2]
+        self.PART_MSG.type = "multipart"
+        self.PART_MSG2.type = "single"
+        self.PART_MSG2.payload = "Email"
+
+        self.assertEqual(mail_2_rmq.get_text(self.MSG), "Email")
+
+    def test_two_part_msg(self):
+
+        """Function:  test_two_part_msg
+
+        Description:  Test two part message is returned.
+
+        Arguments:
+            None
+
+        """
+
+        self.MSG.walk_list = [self.PART_MSG, self.PART_MSG]
+        self.PART_MSG.type = "single"
+        self.PART_MSG.payload = "Email"
+
+        self.assertEqual(mail_2_rmq.get_text(self.MSG), "EmailEmail")
+
+    def test_single_part_msg(self):
+
+        """Function:  test_single_part_msg
+
+        Description:  Test single part message is returned.
+
+        Arguments:
+            None
+
+        """
+
+        self.MSG.walk_list = [self.PART_MSG]
+        self.PART_MSG.type = "single"
+        self.PART_MSG.payload = "Email Message"
+
+        self.assertEqual(mail_2_rmq.get_text(self.MSG), "Email Message")
 
     def test_empty_msg(self):
 
