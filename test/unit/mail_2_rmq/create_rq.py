89554738196819672d6af0f34924502e3b8037ad
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2019-05-09 14:54:18 +0100

    Add test/unit/mail_2_rmq/create_rq.py

---
+++ test/unit/mail_2_rmq/create_rq.py
@@ -0,0 +1,129 @@
+#!/usr/bin/python
+# Classification (U)
+
+"""Program:  create_rq.py
+
+    Description:  Unit testing of create_rq in mail_2_rmq.py.
+
+    Usage:
+        test/unit/mail_2_rmq/create_rq.py
+
+    Arguments:
+        None
+
+"""
+
+# Libraries and Global Variables
+
+# Standard
+import sys
+import os
+
+if sys.version_info < (2, 7):
+    import unittest2 as unittest
+else:
+    import unittest
+
+# Third-party
+import mock
+
+# Local
+sys.path.append(os.getcwd())
+import mail_2_rmq
+import version
+
+# Version
+__version__ = version.__version__
+
+
+class UnitTest(unittest.TestCase):
+
+    """Class:  UnitTest
+
+    Description:  Class which is a representation of a unit testing.
+
+    Super-Class:  unittest.TestCase
+
+    Sub-Classes:  None
+
+    Methods:
+        setUp -> Unit testing initilization.
+        test_create_instance -> Test creating RabbitMQ Instance.
+
+    """
+
+    def setUp(self):
+
+        """Function:  setUp
+
+        Description:  Initialization for unit testing.
+
+        Arguments:
+            None
+
+        """
+
+        class CfgTest(object):
+
+            """Class:  CfgTest
+
+            Description:  Class which is a representation of a cfg module.
+
+            Super-Class:  object
+
+            Sub-Classes:  None
+
+            Methods:
+                __init__ -> Initialize configuration environment.
+
+            """
+
+            def __init__(self):
+
+                """Method:  __init__
+
+                Description:  Initialization instance of the CfgTest class.
+
+                Arguments:
+                        None
+
+                """
+
+                self.user = "USERNAME"
+                self.passwd = "PASSWD"
+                self.host = "HOSTNAME"
+                self.port = 1111
+                self.exchange_name = "EXCHANGE_NAME"
+                self.exchange_type = "EXCAHNGE_TYPE"
+                self.x_durable = True
+                self.q_durable = True
+                self.auto_delete = True
+                self.err_queue = "ERROR_QUEUE"
+                self.valid_queues = ["QUEUE1", "QUEUE2"]
+                self.subj_filter = ["\[.*\]"]
+
+        self.cfg = CfgTest()
+        self.q_name = "Queue_Name"
+        self.r_key = "Routing_Key"
+
+    @mock.patch("mail_2_rmq.rabbitmq_class.RabbitMQPub")
+    def test_create_instance(self, mock_rmq):
+
+        """Function:  test_create_instance
+
+        Description:  Test creating RabbitMQ Instance.
+
+        Arguments:
+            None
+
+        """
+
+        mock_rmq.return_value = "RabbitMQ_Instance"
+
+        self.assertEqual(mail_2_rmq.create_rq(self.cfg, self.q_name,
+                                              self.r_key),
+                         "RabbitMQ_Instance")
+
+
+if __name__ == "__main__":
+    unittest.main()
