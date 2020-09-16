88808cd7d4854c4f43af3a2ae25145a90ee39fb7
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-16 15:51:39 +0100

    Updated test/unit/sonarqube_code_coverage.sh v1.1.0

--- test/unit/sonarqube_code_coverage.sh
+++ test/unit/sonarqube_code_coverage.sh
@@ -8,19 +8,20 @@ coverage erase
 
 echo ""
 echo "Running unit test modules in conjunction with coverage"
+coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/archive_email.py
+coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/camelize.py
+coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/check_nonprocess.py
+coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/connect_process.py
+coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/create_rq.py
+coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/filter_subject.py
+coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/get_text.py
 coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/help_message.py
 coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/load_cfg.py
-coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/camelize.py
-coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/create_rq.py
-coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/parse_email.py
-coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/archive_email.py
-coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/get_text.py
-coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/connect_process.py
-coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/process_message.py
-coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/check_nonprocess.py
-coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/filter_subject.py
-coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/run_program.py
 coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/main.py
+coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/parse_email.py
+coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/process_attach.py
+coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/process_message.py
+coverage run -a --source=mail_2_rmq test/unit/mail_2_rmq/run_program.py
 
 echo ""
 echo "Producing code coverage report"
