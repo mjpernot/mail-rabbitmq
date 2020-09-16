2360f3679423896aa3871631a1c526e10767d88b
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-16 15:51:44 +0100

    Updated test/unit/mail_2_rmq/unit_test_run.sh v1.1.0

--- test/unit/mail_2_rmq/unit_test_run.sh
+++ test/unit/mail_2_rmq/unit_test_run.sh
@@ -4,17 +4,17 @@
 # Will need to run this from the base directory where the class file 
 #   is located at.
 
-test/unit/mail_2_rmq/load_cfg.py
-test/unit/mail_2_rmq/camelize.py
-test/unit/mail_2_rmq/create_rq.py
-test/unit/mail_2_rmq/parse_email.py
 test/unit/mail_2_rmq/archive_email.py
-test/unit/mail_2_rmq/get_text.py
-test/unit/mail_2_rmq/connect_process.py
-test/unit/mail_2_rmq/process_message.py
+test/unit/mail_2_rmq/camelize.py
 test/unit/mail_2_rmq/check_nonprocess.py
+test/unit/mail_2_rmq/connect_process.py
+test/unit/mail_2_rmq/create_rq.py
 test/unit/mail_2_rmq/filter_subject.py
+test/unit/mail_2_rmq/get_text.py
 test/unit/mail_2_rmq/help_message.py
-test/unit/mail_2_rmq/run_program.py
+test/unit/mail_2_rmq/load_cfg.py
 test/unit/mail_2_rmq/main.py
-
+test/unit/mail_2_rmq/parse_email.py
+test/unit/mail_2_rmq/process_attach.py
+test/unit/mail_2_rmq/process_message.py
+test/unit/mail_2_rmq/run_program.py
