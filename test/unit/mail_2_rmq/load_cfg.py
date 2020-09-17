489554a05929c25d5607f7ff7492d3f2e1b5c6aa
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2020-09-17 13:27:47 +0100

    Updated test/unit/mail_2_rmq/load_cfg.py v1.1.0

--- test/unit/mail_2_rmq/load_cfg.py
+++ test/unit/mail_2_rmq/load_cfg.py
@@ -34,6 +34,32 @@ import version
 __version__ = version.__version__
 
 
+class CfgTest(object):
+
+    """Class:  CfgTest
+
+    Description:  Class which is a representation of a cfg module.
+
+    Methods:
+        __init__ -> Initialize configuration environment.
+
+    """
+
+    def __init__(self):
+
+        """Method:  __init__
+
+        Description:  Initialization instance of the CfgTest class.
+
+        Arguments:
+
+        """
+
+        self.email_dir = "Email_Directory"
+        self.log_file = "Log_Directory"
+        self.tmp_dir = "Tmp_Directory"
+
+
 class UnitTest(unittest.TestCase):
 
     """Class:  UnitTest
@@ -42,6 +68,12 @@ class UnitTest(unittest.TestCase):
 
     Methods:
         setUp -> Unit testing initilization.
+        test_tmp_dir_false -> Test with tmp_dir check failure.
+        test_tmp_dir_true -> Test with tmp_dir check success.
+        test_log_file_false -> Test with log_file check failure.
+        test_log_file_true -> Test with log_file check success.
+        test_email_dir_false -> Test with email_dir check failure.
+        test_email_dir_true -> Test with email_dir check success.
         test_false_false_cfg -> Test False and False statuses.
         test_false_true_cfg -> Test False and True statuses.
         test_true_false_cfg -> Test True and False statuses.
@@ -59,33 +91,116 @@ class UnitTest(unittest.TestCase):
 
         """
 
-        class CfgTest(object):
-
-            """Class:  CfgTest
-
-            Description:  Class which is a representation of a cfg module.
-
-            Methods:
-                __init__ -> Initialize configuration environment.
-
-            """
-
-            def __init__(self):
-
-                """Method:  __init__
-
-                Description:  Initialization instance of the CfgTest class.
-
-                Arguments:
-
-                """
-
-                self.email_dir = "Email_Directory"
-                self.log_file = "Log_Directory"
-
         self.cfg = CfgTest()
         self.cfg_name = "Configuration_File"
         self.cfg_dir = "Configuration_Directory"
+        self.results = [[True, ""], [True, ""], [True, ""]]
+        self.results2 = [[False, ""], [False, ""], [False, ""]]
+        self.results3 = [[False, ""], [True, ""], [True, ""]]
+        self.results4 = [[True, ""], [False, ""], [True, ""]]
+        self.results5 = [[True, ""], [True, ""], [False, ""]]
+
+    @mock.patch("mail_2_rmq.gen_libs")
+    def test_tmp_dir_false(self, mock_lib):
+
+        """Function:  test_tmp_dir_false
+
+        Description:  Test with tmp_dir check failure.
+
+        Arguments:
+
+        """
+
+        mock_lib.load_module.return_value = self.cfg
+        mock_lib.chk_crt_dir.side_effect = self.results5
+
+        self.assertEqual(mail_2_rmq.load_cfg(self.cfg_name, self.cfg_dir),
+                         (self.cfg, False))
+
+    @mock.patch("mail_2_rmq.gen_libs")
+    def test_tmp_dir_true(self, mock_lib):
+
+        """Function:  test_tmp_dir_true
+
+        Description:  Test with tmp_dir check success.
+
+        Arguments:
+
+        """
+
+        mock_lib.load_module.return_value = self.cfg
+        mock_lib.chk_crt_dir.side_effect = self.results
+
+        self.assertEqual(mail_2_rmq.load_cfg(self.cfg_name, self.cfg_dir),
+                         (self.cfg, True))
+
+    @mock.patch("mail_2_rmq.gen_libs")
+    def test_log_file_false(self, mock_lib):
+
+        """Function:  test_log_file_false
+
+        Description:  Test with log_file check failure.
+
+        Arguments:
+
+        """
+
+        mock_lib.load_module.return_value = self.cfg
+        mock_lib.chk_crt_dir.side_effect = self.results4
+
+        self.assertEqual(mail_2_rmq.load_cfg(self.cfg_name, self.cfg_dir),
+                         (self.cfg, False))
+
+    @mock.patch("mail_2_rmq.gen_libs")
+    def test_log_file_true(self, mock_lib):
+
+        """Function:  test_log_file_true
+
+        Description:  Test with log_file check success.
+
+        Arguments:
+
+        """
+
+        mock_lib.load_module.return_value = self.cfg
+        mock_lib.chk_crt_dir.side_effect = self.results
+
+        self.assertEqual(mail_2_rmq.load_cfg(self.cfg_name, self.cfg_dir),
+                         (self.cfg, True))
+
+    @mock.patch("mail_2_rmq.gen_libs")
+    def test_email_dir_false(self, mock_lib):
+
+        """Function:  test_email_dir_false
+
+        Description:  Test with email_dir check failure.
+
+        Arguments:
+
+        """
+
+        mock_lib.load_module.return_value = self.cfg
+        mock_lib.chk_crt_dir.side_effect = self.results3
+
+        self.assertEqual(mail_2_rmq.load_cfg(self.cfg_name, self.cfg_dir),
+                         (self.cfg, False))
+
+    @mock.patch("mail_2_rmq.gen_libs")
+    def test_email_dir_true(self, mock_lib):
+
+        """Function:  test_email_dir_true
+
+        Description:  Test with email_dir check success.
+
+        Arguments:
+
+        """
+
+        mock_lib.load_module.return_value = self.cfg
+        mock_lib.chk_crt_dir.side_effect = self.results
+
+        self.assertEqual(mail_2_rmq.load_cfg(self.cfg_name, self.cfg_dir),
+                         (self.cfg, True))
 
     @mock.patch("mail_2_rmq.gen_libs")
     def test_false_false_cfg(self, mock_lib):
@@ -99,7 +214,7 @@ class UnitTest(unittest.TestCase):
         """
 
         mock_lib.load_module.return_value = self.cfg
-        mock_lib.chk_crt_dir.side_effect = [[False, ""], [False, ""]]
+        mock_lib.chk_crt_dir.side_effect = self.results2
 
         self.assertEqual(mail_2_rmq.load_cfg(self.cfg_name, self.cfg_dir),
                          (self.cfg, False))
@@ -116,7 +231,7 @@ class UnitTest(unittest.TestCase):
         """
 
         mock_lib.load_module.return_value = self.cfg
-        mock_lib.chk_crt_dir.side_effect = [[False, ""], [True, ""]]
+        mock_lib.chk_crt_dir.side_effect = self.results3
 
         self.assertEqual(mail_2_rmq.load_cfg(self.cfg_name, self.cfg_dir),
                          (self.cfg, False))
@@ -133,7 +248,7 @@ class UnitTest(unittest.TestCase):
         """
 
         mock_lib.load_module.return_value = self.cfg
-        mock_lib.chk_crt_dir.side_effect = [[True, ""], [False, ""]]
+        mock_lib.chk_crt_dir.side_effect = self.results4
 
         self.assertEqual(mail_2_rmq.load_cfg(self.cfg_name, self.cfg_dir),
                          (self.cfg, False))
@@ -150,7 +265,7 @@ class UnitTest(unittest.TestCase):
         """
 
         mock_lib.load_module.return_value = self.cfg
-        mock_lib.chk_crt_dir.side_effect = [[True, ""], [True, ""]]
+        mock_lib.chk_crt_dir.side_effect = self.results
 
         self.assertEqual(mail_2_rmq.load_cfg(self.cfg_name, self.cfg_dir),
                          (self.cfg, True))
