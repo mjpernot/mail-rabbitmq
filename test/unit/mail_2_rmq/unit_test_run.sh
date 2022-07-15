36d8d5cdc8c8c1c3267193f9a462f7a11f2b06e4
Mark Pernot <mark.pernot@gmail.com>
2022-07-15 10:14:46 +0100

    Update unit test run modules v1.3.1

--- test/unit/mail_2_rmq/unit_test_run.sh
+++ test/unit/mail_2_rmq/unit_test_run.sh
@@ -8,7 +8,6 @@ test/unit/mail_2_rmq/archive_email.py
 test/unit/mail_2_rmq/check_nonprocess.py
 test/unit/mail_2_rmq/connect_process.py
 test/unit/mail_2_rmq/filter_subject.py
-test/unit/mail_2_rmq/get_email_addr.py
 test/unit/mail_2_rmq/get_text.py
 test/unit/mail_2_rmq/help_message.py
 test/unit/mail_2_rmq/load_cfg.py
