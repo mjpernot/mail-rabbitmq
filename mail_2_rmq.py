270a806724588ccb1d4b54f3ad7781b7b46c7628
mark.j.pernot <mark.j.pernot.ctr@mail.mil>
2025-09-16 13:06:58 +0100

    Modified a number of _debug functions v2.2.2

--- mail_2_rmq.py
+++ mail_2_rmq.py
@@ -906,8 +906,6 @@ def process_from_debug(cfg, log, msg, from_addr):
             log.log_info(
                 f"[{os.getpid()}] Valid From address:"
                 f" {from_addr} with file attachment: {fname}")
-
-################################################################################
             log.log_debug(
                 f"[{os.getpid()}] process_from: Calling pub_to_rmq_debug")
             pub_to_rmq_debug(
@@ -915,31 +913,6 @@ def process_from_debug(cfg, log, msg, from_addr):
                 cfg.debug_queue_dict[from_addr], msg, fname)
             log.log_debug(
                 f"[{os.getpid()}] process_from: Finished pub_to_rmq_debug")
-################################################################################
-#            log.log_debug(
-#                f"[{os.getpid()}] process_from: Calling connect_rmq_debug")
-#            connect_rmq_debug(
-#                cfg, log, cfg.debug_queue_dict[from_addr],
-#                cfg.debug_queue_dict[from_addr], msg, fname=fname)
-#            log.log_debug(
-#                f"[{os.getpid()}] process_from: Finished connect_rmq_debug")
-#            log.log_debug(
-#                f"[{os.getpid()}] process_from: Removing file: {fname}")
-#            err_flag, err_msg = gen_libs.rm_file(fname)
-#            log.log_debug(
-#                f"[{os.getpid()}] process_from: Removed file {fname}")
-#
-#            if err_flag:
-#                log.log_debug(
-#                    f"[{os.getpid()}] process_from: File {fname}, Perms:"
-#                    f" {oct(os.stat(fname).st_mode)[-3:]}")
-#                log.log_debug(
-#                    f"[{os.getpid()}] process_from: File Owner:"
-#                    f" {os.stat(fname).st_uid}")
-#                log.log_warn(
-#                    f"[{os.getpid()}] process_from: Message: {err_msg}")
-################################################################################
-
             log.log_debug(
                 f"[{os.getpid()}] process_from: Bottom of fname_list loop")
 
@@ -1022,36 +995,11 @@ def process_file_debug(cfg, log, subj, msg):
                 f"[{os.getpid()}] process_file: Top of fname_list loop")
             log.log_info(
                 f"[{os.getpid()}] Valid subject with file attachment: {fname}")
-
-################################################################################
             log.log_debug(
                 f"[{os.getpid()}] process_file: Calling pub_to_rmq_debug")
             pub_to_rmq_debug(cfg, log, subj, subj, msg, fname)
             log.log_debug(
                 f"[{os.getpid()}] process_file: Finished pub_to_rmq_debug")
-################################################################################
-#            log.log_debug(
-#                f"[{os.getpid()}] process_file: Calling connect_rmq_debug")
-#            connect_rmq_debug(cfg, log, subj, subj, msg, fname=fname)
-#            log.log_debug(
-#                f"[{os.getpid()}] process_file: Finished connect_rmq_debug")
-#            log.log_debug(
-#                f"[{os.getpid()}] process_file: Removing file: {fname}")
-#            err_flag, err_msg = gen_libs.rm_file(fname)
-#            log.log_debug(
-#                f"[{os.getpid()}] process_file: Removed file {fname}")
-#
-#            if err_flag:
-#                log.log_debug(
-#                    f"[{os.getpid()}] process_file: File {fname}, Perms:"
-#                    f" {oct(os.stat(fname).st_mode)[-3:]}")
-#                log.log_debug(
-#                    f"[{os.getpid()}] process_file: File Owner:"
-#                    f" {os.stat(fname).st_uid}")
-#                log.log_warn(
-#                    f"[{os.getpid()}] process_file: Message: {err_msg}")
-################################################################################
-
             log.log_debug(
                 f"[{os.getpid()}] process_file: Bottom of fname_list loop")
 
@@ -1063,40 +1011,12 @@ def process_file_debug(cfg, log, subj, msg):
             log.log_debug(f"[{os.getpid()}] Top of fname_list second loop")
             log.log_info(
                 f"[{os.getpid()}] Invalid subject with file attached: {fname}")
-
-################################################################################
             log.log_debug(
                 f"[{os.getpid()}] process_file 2: Calling pub_to_rmq_debug")
             pub_to_rmq_debug(
                 cfg, log, cfg.err_file_queue, cfg.err_file_queue, msg, fname)
             log.log_debug(
                 f"[{os.getpid()}] process_file 2: Finished pub_to_rmq_debug")
-################################################################################
-#            log.log_debug(
-#                f"[{os.getpid()}] process_file: Calling connect_rmq_debug:"
-#                " error2")
-#            connect_rmq_debug(
-#                cfg, log, cfg.err_file_queue, cfg.err_file_queue, msg,
-#                fname=fname)
-#            log.log_debug(
-#                f"[{os.getpid()}] process_file: Finished connect_rmq_debug:"
-#                " error2")
-#            log.log_debug(
-#                f"[{os.getpid()}] process_file: Removing file: {fname}")
-#            err_flag, err_msg = gen_libs.rm_file(fname)
-#            log.log_debug(
-#                f"[{os.getpid()}] process_file: Removed file {fname}")
-#
-#            if err_flag:
-#                log.log_debug(
-#                    f"[{os.getpid()}] process_file: File {fname}, Perms:"
-#                    f" {oct(os.stat(fname).st_mode)[-3:]}")
-#                log.log_debug(
-#                    f"[{os.getpid()}] File Owner: {os.stat(fname).st_uid}")
-#                log.log_warn(
-#                    f"[{os.getpid()}] process_file 2: Message: {err_msg}")
-################################################################################
-
             log.log_debug(f"[{os.getpid()}] Bottom of fname_list second loop")
 
     else:
